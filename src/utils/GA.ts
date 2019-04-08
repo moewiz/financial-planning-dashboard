03b9eb24208df92722523e816c827f92566f3e3e
Khoa Nguyen <meo.wizard@gmail.com>
2019-04-08 11:58:45 +0700

    Send GA events:
    - sendGAEventNewClientCreated.
    - sendGAEventCreateClientButtonClicked.

--- src/utils/GA.ts
+++ src/utils/GA.ts
@@ -1,6 +1,8 @@
 import ReactGA from 'react-ga';
 import { isLocalhost } from '../serviceWorker';
 
+declare type EventCategory = 'new_client';
+declare type EventAction = 'create_button_clicked' | 'client_saved';
 const GOOGLE_ANALYTICS_ID: string = 'UA-137783509-1';
 
 // Initialize Google Analytics
@@ -10,9 +12,32 @@ function initializeGASession(): void {
   return ReactGA.initialize(GOOGLE_ANALYTICS_ID, { debug: isDevelop });
 }
 
-// "Create client" button clicked
-function sendGAEvent() {
-
+/**
+ * Send GA Event creator
+ */
+function sendGAEvent({ category, action }: { category: EventCategory; action: EventAction; }) {
+  return ReactGA.event({
+    category,
+    action,
+  });
 }
 
-export { initializeGASession };
+/**
+ * "Create client" button clicked
+ * event_category = new_client
+ * event_label = create_button_clicked
+ */
+function sendGAEventCreateClientButtonClicked(): void {
+  return sendGAEvent({ category: 'new_client', action: 'create_button_clicked' });
+}
+
+/**
+ * New client created
+ * event_category = new_client
+ * event_label = client_saved
+ */
+function sendGAEventNewClientCreated(): void {
+  return sendGAEvent({ category: 'new_client', action: 'client_saved' });
+}
+
+export { initializeGASession, sendGAEventCreateClientButtonClicked, sendGAEventNewClientCreated };
