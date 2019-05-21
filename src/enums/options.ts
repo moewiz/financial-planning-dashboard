1ab296740ef11515468c389c707895d1380b932d
Khoa Nguyen <meo.wizard@gmail.com>
2019-05-21 10:50:12 +0700

    Add Select options for Insurance - Cover Details and Premium & Fee Details.

--- src/enums/options.ts
+++ src/enums/options.ts
@@ -2,9 +2,9 @@ function mapOptionObjectToArray(object: { [key: string]: any }) {
   return Object.entries(object).map(([key, value]) => ({ value: key, label: value }));
 }
 
-function createIndexationOptions(step = 0.5): object {
+function createRateOptions(step: number = 0.5, max: number = 10): object {
   const options: { [key: string]: any } = {};
-  for (let i = 0; i <= 10; i = i + step) {
+  for (let i = 0; i <= max; i = i + step) {
     options[`${i}%`] = `${i}%`;
   }
   return options;
@@ -28,6 +28,13 @@ export const OWNER = {
 };
 export const ownerOptions = mapOptionObjectToArray(OWNER);
 
+export const OWNER_WITH_JOINT = {
+  client: 'Client',
+  partner: 'Partner',
+  joint: 'Joint',
+};
+export const ownerWithJointOptions = mapOptionObjectToArray(OWNER_WITH_JOINT);
+
 export const EMP_STATUS = {
   employed: 'Employed',
   selfEmployed: 'Self Employed',
@@ -46,23 +53,11 @@ export const RISK_PROFILE = {
 };
 export const riskProfileOptions = mapOptionObjectToArray(RISK_PROFILE);
 
-export const DID_OR_NOT = {
-  true: 'did',
-  false: 'did not',
-};
-export const didOrNotOptions = mapOptionObjectToArray(DID_OR_NOT);
+export const didOrNotOptions = [{ value: true, label: 'did' }, { value: false, label: 'did not' }];
 
-export const LOOKING_FOR_COUPLE_ADVICE = {
-  true: 'couple',
-  false: 'single',
-};
-export const lookingForCoupleAdviceOptions = mapOptionObjectToArray(LOOKING_FOR_COUPLE_ADVICE);
+export const lookingForCoupleAdviceOptions = [{ value: true, label: 'couple' }, { value: false, label: 'single' }];
 
-export const IS_OR_NOT = {
-  true: 'is',
-  false: 'is not',
-};
-export const isOrNotOptions = mapOptionObjectToArray(IS_OR_NOT);
+export const isOrNotOptions = [{ value: true, label: 'is' }, { value: false, label: 'is not' }];
 
 export const FROM_1 = {
   start: 'Start',
@@ -111,7 +106,7 @@ export const expenditureTypeOptions = mapOptionObjectToArray(EXPENDITURE_TYPES);
 export const INDEXATION_OPTIONS = {
   salaryInflation: 'Salary Inflation',
   inflationCPI: 'Inflation (CPI)',
-  ...createIndexationOptions(),
+  ...createRateOptions(),
   otherNonTaxable: 'Other Non-Taxable',
 };
 export const indexationOptions = mapOptionObjectToArray(INDEXATION_OPTIONS);
@@ -140,14 +135,59 @@ export const LIABILITIES_TYPES = {
 };
 export const liabilitiesTypes = mapOptionObjectToArray(LIABILITIES_TYPES);
 
-export const YES_NO = {
-  true: 'Yes',
-  false: 'No',
-};
-export const yesNoOptions = mapOptionObjectToArray(YES_NO);
+export const yesNoOptions = [{ value: true, label: 'Yes' }, { value: false, label: 'No' }];
 
 export const SG_RATE = {
   sgc: 'SGC',
-  ...createIndexationOptions(1),
+  ...createRateOptions(1, 15),
 };
 export const sgRateOptions = mapOptionObjectToArray(SG_RATE);
+
+export const COVER_TYPE = {
+  life: 'Life',
+  tpd: 'TPD',
+  trauma: 'Trauma',
+  incomeProtection: 'Income Protection',
+};
+export const coverTypeOptions = mapOptionObjectToArray(COVER_TYPE);
+
+export const POLICY_OWNER = {
+  superFund: 'SuperFund',
+  client: 'Client',
+  partner: 'Partner',
+};
+export const policyOwnerOptions = mapOptionObjectToArray(POLICY_OWNER);
+
+export const TPD_TYPE = {
+  any: 'Any',
+  own: 'Own',
+  nonWorking: 'Non Working',
+};
+export const tpdTypeOptions = mapOptionObjectToArray(TPD_TYPE);
+
+export const INCOME_PROTECTION_TYPE = {
+  agreedValue: 'Agreed Value',
+  indemnity: 'Indemnity',
+};
+export const incomeProtectionTypeOptions = mapOptionObjectToArray(INCOME_PROTECTION_TYPE);
+
+export const WAITING_PERIOD_TYPE = {
+  days: 'Days',
+  months: 'Months',
+  years: 'Years',
+};
+export const waitingPeriodTypeOptions = mapOptionObjectToArray(WAITING_PERIOD_TYPE);
+
+export const FEE_TYPE = {
+  premium: 'Premium',
+  policyFee: 'Policy Fee',
+  stampDuty: 'Stamp Duty',
+  other: 'Other',
+}
+export const feeTypeOptions = mapOptionObjectToArray(FEE_TYPE);
+
+export const FREQUENCY = {
+  yearly: 'Yearly',
+  monthly: 'Monthly',
+}
+export const frequencyOptions = mapOptionObjectToArray(FREQUENCY);
