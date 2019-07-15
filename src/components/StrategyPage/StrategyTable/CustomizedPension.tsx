e8208cb2650fb7e9e5b25a6c7499af0842728bd2
Khoa Nguyen <meo.wizard@gmail.com>
2019-07-15 18:10:33 +0700

    Load Full value based on seleted option in dropdown.

---
+++ src/components/StrategyPage/StrategyTable/CustomizedPension.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { DrawerTableRows, FullyCustomized } from '../Drawer/styled';
+import EditCell, { EditCellType } from '../Drawer/EditCell';
+import { get, head, isString, find, map, replace, slice, trim, random } from 'lodash';
+import { getOptions, StrategyItemProps } from './StrategyItem';
+
+const defaultFullValues = [1000, 2000, 3000, 4000];
+
+const CustomizedPension = (
+  props: StrategyItemProps & { name: string; context: string; sentenceKey: string; defaultFullValue: number },
+) => {
+  const {
+    name,
+    context,
+    client,
+    partner,
+    sentenceKey,
+    strategy,
+    strategyIndex,
+    strategyType,
+    defaultFullValue,
+  } = props;
+  const superannuationOptions = map(
+    getOptions(context, { client, partner }, 'superannuation'),
+    (option, index: number) => ({
+      ...option,
+      fullValue: get(defaultFullValues, index, 0),
+    }),
+  );
+  superannuationOptions.push({ value: 'customisedRollover', label: 'Customised Rollover' });
+  const title = sentenceKey === 'commenceAccount' ? 'an account based pension' : 'a TTR pension';
+  const superValue = get(strategy, 'values[1]');
+  const isCustomisedRollover = superValue === 'customisedRollover';
+  const [fullValue, setDefaultFullValue] = React.useState<number>(
+    get(find(superannuationOptions, { value: superValue }), 'fullValue', defaultFullValue),
+  );
+  const updateFullValue = (value: any) => {
+    if (value !== 'customisedRollover') {
+      // load full value from Superannuation option
+      const superFullValue = get(find(superannuationOptions, { value }), 'fullValue', defaultFullValue);
+      setDefaultFullValue(superFullValue);
+    } else {
+      // load full value from API
+      setDefaultFullValue(defaultFullValue);
+    }
+  };
+
+  return (
+    <FullyCustomized>
+      {name}, commence {title} in
+      <DrawerTableRows noBorder className={'strategy-item'}>
+        <EditCell
+          name={`${strategyType}.strategies[${strategyIndex}].values[0]`}
+          type={EditCellType.date}
+          value={get(strategy, 'values[0]')}
+          onChange={(val) => {
+            console.log(val);
+          }}
+        />
+      </DrawerTableRows>
+      <span>{isCustomisedRollover ? 'as a' : 'from your'}</span>
+      <EditCell
+        name={`${strategyType}.strategies[${strategyIndex}].values[1]`}
+        value={superValue}
+        type={EditCellType.select}
+        options={superannuationOptions}
+        onChange={updateFullValue}
+      />
+      <span>
+        with{' '}
+        {isCustomisedRollover ? (
+          <b>{get(strategy, ['values', 1], 0)}</b>
+        ) : (
+          <EditCell
+            name={`${strategyType}.strategies[${strategyIndex}].values[2]`}
+            value={get(strategy, 'values[2]')}
+            type={EditCellType.dropdownFreeText}
+            onChange={(val) => console.log(val)}
+            defaultFullValue={fullValue}
+          />
+        )}
+      </span>
+      <span>Drawdown pension income of $xx per month/annum</span>
+    </FullyCustomized>
+  );
+};
+
+export default CustomizedPension;
