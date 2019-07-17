8e7803f4675a2cd4184cfd1e58f2afcbdb367cba
Khoa Nguyen <meo.wizard@gmail.com>
2019-07-17 14:47:46 +0700

    Get a fake token.

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -40,6 +40,7 @@ class ApiUtils {
 
   public static getAccessToken(): string | undefined {
     const rootState = store.getState();
+    return 'hello';
     return rootState.auth && rootState.auth.get('token');
   }
 
