06335e77b92d088e0f5c4890a3908ab3bf40d9af
Khoa Nguyen <meo.wizard@gmail.com>
2019-09-16 18:08:28 +0700

    The title of the pull out drawer reflects to which product user selects
    - Prepare the data for searching fund

--- src/components/ProductOptimizer/Drawer/CustomSearch.tsx
+++ src/components/ProductOptimizer/Drawer/CustomSearch.tsx
@@ -5,12 +5,16 @@ const { Option, OptGroup } = Select;
 import { TopSearch } from '../../../layouts/Header/styled';
 import { Filter } from '../../Icons';
 
+export type CustomSearchType = 'product' | 'fund';
+
 interface Prop {
   placeholder: string;
+  type?: CustomSearchType;
+  onSelect?: (value: any, option: any) => void;
 }
 
 interface State {
-  keyword: string;
+  keyword?: string;
 }
 
 class CustomSearch extends PureComponent<Prop, State> {
@@ -26,8 +30,105 @@ class CustomSearch extends PureComponent<Prop, State> {
     this.setState({ keyword });
   }
 
+  public renderResults = () => {
+    const { type } = this.props;
+
+    if (type === 'fund') {
+      return (
+        <OptGroup label="Search result">
+          <Option value={JSON.stringify({ id: 1, name: 'Fund DE' })} title="Fund DE">
+            Fund DE
+          </Option>
+          <Option value={JSON.stringify({ id: 2, name: 'Fund DF' })} title="Fund DF">
+            Fund DF
+          </Option>
+          <Option value={JSON.stringify({ id: 3, name: 'Fund DG' })} title="Fund DG">
+            Fund DG
+          </Option>
+          <Option value={JSON.stringify({ id: 4, name: 'Fund DH' })} title="Fund DH">
+            Fund DH
+          </Option>
+          <Option value={JSON.stringify({ id: 5, name: 'Fund DI' })} title="Fund DI">
+            Fund DI
+          </Option>
+        </OptGroup>
+      );
+    }
+
+    return [
+      <OptGroup label="Recent">
+        <Option
+          value={JSON.stringify({ id: 1, name: 'BlackRock Global Income' })}
+          title="BlackRock Global Income (AMP1995AU)"
+        >
+          <span className="text">BlackRock Global Income</span>
+          <span className="code">AMP1995AU</span>
+        </Option>
+        <Option value={JSON.stringify({ id: 2, name: 'Product X' })} title="Product X (AMP1707AU)">
+          <span className="text">Product X</span>
+          <span className="code">AMP1707AU</span>
+        </Option>
+      </OptGroup>,
+      <OptGroup label="Popular">
+        <Option
+          value={JSON.stringify({ id: 3, name: 'AMP Australian Share Index' })}
+          title="AMP Australian Share Index (AMP1995AU)"
+        >
+          <span className="text">AMP Australian Share Index</span>
+          <span className="code">AMP1995AU</span>
+        </Option>
+        <Option
+          value={JSON.stringify({ id: 4, name: 'Perpetual Industrial Share' })}
+          title="Perpetual Industrial Share (AMP0057AU)"
+        >
+          <span className="text">Perpetual Industrial Share</span>
+          <span className="code">AMP0057AU</span>
+        </Option>
+        <Option
+          value={JSON.stringify({ id: 5, name: 'Plato Australian Shares Income' })}
+          title="Plato Australian Shares Income (AMP0767AU)"
+        >
+          <span className="text">Plato Australian Shares Income</span>
+          <span className="code">AMP0767AU</span>
+        </Option>
+      </OptGroup>,
+      <OptGroup label="Model Portfolios">
+        <Option
+          value={JSON.stringify({ id: 6, name: 'Model Portpolio 1 (Growth)' })}
+          title="Model Portpolio 1 (Growth)"
+        >
+          Model Portpolio 1 (Growth)
+        </Option>
+        <Option
+          value={JSON.stringify({ id: 7, name: 'Model Portpolio 2 (Defensive)' })}
+          title="Model Portpolio 2 (Defensive)"
+        >
+          Model Portpolio 2 (Defensive)
+        </Option>
+        <Option
+          value={JSON.stringify({ id: 8, name: 'Model Portpolio 2 (Moderate)' })}
+          title="Model Portpolio 3 (Moderate)"
+        >
+          Model Portpolio 3 (Moderate)
+        </Option>
+        <Option
+          value={JSON.stringify({ id: 9, name: 'Model Portpolio 2 (Defensive)' })}
+          title="Model Portpolio 4 (Defensive)"
+        >
+          Model Portpolio 4 (Defensive)
+        </Option>
+        <Option
+          value={JSON.stringify({ id: 10, name: 'Model Portpolio 2 (Growth)' })}
+          title="Model Portpolio 5 (Growth)"
+        >
+          Model Portpolio 5 (Growth)
+        </Option>
+      </OptGroup>,
+    ];
+  }
+
   public render() {
-    const { placeholder } = this.props;
+    const { placeholder, onSelect } = this.props;
     const { keyword } = this.state;
 
     return (
@@ -37,42 +138,14 @@ class CustomSearch extends PureComponent<Prop, State> {
           showSearch
           value={keyword}
           onChange={this.onChange}
+          onSelect={onSelect}
           placeholder={placeholder}
           className="custom-select"
           showArrow={false}
           dropdownClassName="custom-search-menu"
+          optionFilterProp="title"
         >
-          <OptGroup label="Recent">
-            <Option value="BlackRock Global Income AMP1995AU">
-              <span className="text">BlackRock Global Income</span>
-              <span className="code">AMP1995AU</span>
-            </Option>
-            <Option value="Product X AMP1707AU">
-              <span className="text">Product X</span>
-              <span className="code">AMP1707AU</span>
-            </Option>
-          </OptGroup>
-          <OptGroup label="Popular">
-            <Option value="AMP Australian Share Index AMP1995AU">
-              <span className="text">AMP Australian Share Index</span>
-              <span className="code">AMP1995AU</span>
-            </Option>
-            <Option value="Perpetual Industrial Share AMP0057AU">
-              <span className="text">Perpetual Industrial Share</span>
-              <span className="code">AMP0057AU</span>
-            </Option>
-            <Option value="Plato Australian Shares Income AMP0767AU">
-              <span className="text">Plato Australian Shares Income</span>
-              <span className="code">AMP0767AU</span>
-            </Option>
-          </OptGroup>
-          <OptGroup label="Model Portfolios">
-            <Option value="Model Portpolio 1 (Growth)">Model Portpolio 1 (Growth)</Option>
-            <Option value="Model Portpolio 2 (Defensive)">Model Portpolio 2 (Defensive)</Option>
-            <Option value="Model Portpolio 3 (Moderate)">Model Portpolio 3 (Moderate)</Option>
-            <Option value="Model Portpolio 4 (Defensive)">Model Portpolio 4 (Defensive)</Option>
-            <Option value="Model Portpolio 5 (Growth)">Model Portpolio 5 (Growth)</Option>
-          </OptGroup>
+          {this.renderResults()}
         </Select>
         <Icon component={Filter} />
       </TopSearch>
--- src/components/ProductOptimizer/Drawer/DrawerProduct.tsx
+++ src/components/ProductOptimizer/Drawer/DrawerProduct.tsx
@@ -8,17 +8,32 @@ import FundTable from './FundTable';
 import { DrawerProductWrapper, FundBlock, FundTabContent, HorizontalScrollable } from '../styled';
 import CustomSearch from './CustomSearch';
 
+export interface Option {
+  id: number;
+  name: string;
+  code?: string;
+}
+
 export interface Product {
   id?: number;
   description: string;
   value: number | string;
   links?: Product[];
+  details?: {
+    product: Option;
+    funds: Option[];
+  };
 }
 
 interface DrawerProductProps {
   isOpen: boolean;
   close: () => void;
-  product?: any;
+  product?: Product;
+}
+
+interface DrawerProductStates {
+  hovering?: number;
+  title: string;
 }
 
 const data = [
@@ -29,8 +44,8 @@ const data = [
   },
 ];
 
-class DrawerProduct extends PureComponent<DrawerProductProps> {
-  public state = { hovering: -1 };
+class DrawerProduct extends PureComponent<DrawerProductProps, DrawerProductStates> {
+  public state = { hovering: -1, title: '' };
   public columns = [
     {
       title: 'Fund Name',
@@ -53,6 +68,27 @@ class DrawerProduct extends PureComponent<DrawerProductProps> {
     },
   ];
 
+  public componentDidUpdate(
+    prevProps: Readonly<DrawerProductProps>,
+    prevState: Readonly<DrawerProductStates>,
+    snapshot?: any,
+  ): void {
+    const { isOpen } = this.props;
+
+    if (isOpen !== prevProps.isOpen && isOpen) {
+      this.setTitle();
+    }
+  }
+
+  public setTitle = () => {
+    const { product } = this.props;
+    if (product && product.details && product.details.product.name) {
+      this.setState({ title: product.details.product.name });
+    } else {
+      this.setState({ title: '' });
+    }
+  }
+
   public renderDrawer = () => {
     const { product } = this.props;
     if (product && product.links && product.links.length > 0) {
@@ -61,16 +97,26 @@ class DrawerProduct extends PureComponent<DrawerProductProps> {
 
     return this.renderNewDrawer();
   }
+
   public mouseOver = (id?: number) => {
-   this.setState({
-    hovering: id,
-   });
+    this.setState({
+      hovering: id,
+    });
   }
+
   public mouseOut = (id?: number) => {
     this.setState({
       hovering: -1,
     });
   }
+
+  public onSelectProduct = (value: any) => {
+    const option: Option = JSON.parse(value);
+    if (option && option.name) {
+      this.setState({ title: option.name });
+    }
+  }
+
   public renderFundTab = () => {
     const { product } = this.props;
     const { hovering } = this.state;
@@ -88,25 +134,34 @@ class DrawerProduct extends PureComponent<DrawerProductProps> {
       <>
         <FundTabContent>
           <FundBlock
-             className={getClasses(-2)}
-             onMouseOver={() => { this.mouseOver(-2); }}
-             onMouseOut={() => { this.mouseOut(-2); }}
+            className={getClasses(-2)}
+            onMouseOver={() => {
+              this.mouseOver(-2);
+            }}
+            onMouseOut={() => {
+              this.mouseOut(-2);
+            }}
           >
-            <CustomSearch placeholder="Search Product" />
-            <CustomSearch placeholder="Search Proposed Fund" />
+            <CustomSearch placeholder="Add Product" />
+            <CustomSearch placeholder="Search Proposed Fund" type="fund" />
           </FundBlock>
           <HorizontalScrollable>
-            {map(product.links, (linkedProduct: Product) => (
-              <FundBlock
-                className={getClasses(linkedProduct.id)}
-                key={linkedProduct.id}
-                onMouseOver={() => { this.mouseOver(linkedProduct.id); }}
-                onMouseOut={() => { this.mouseOut(linkedProduct.id); }}
-              >
-                <CustomSearch placeholder="Search Product" />
-                <CustomSearch placeholder="Search Proposed Fund" />
-              </FundBlock>
-            ))}
+            {product &&
+              map(product.links, (linkedProduct: Product) => (
+                <FundBlock
+                  className={getClasses(linkedProduct.id)}
+                  key={linkedProduct.id}
+                  onMouseOver={() => {
+                    this.mouseOver(linkedProduct.id);
+                  }}
+                  onMouseOut={() => {
+                    this.mouseOut(linkedProduct.id);
+                  }}
+                >
+                  <CustomSearch placeholder="Add Product" />
+                  <CustomSearch placeholder="Search Proposed Fund" />
+                </FundBlock>
+              ))}
           </HorizontalScrollable>
         </FundTabContent>
 
@@ -145,12 +200,14 @@ class DrawerProduct extends PureComponent<DrawerProductProps> {
   }
 
   public renderNewDrawer = () => {
+    const { title } = this.state;
+
     return (
       <DrawerProductWrapper>
-        <DrawerTitle>My Product</DrawerTitle>
+        <DrawerTitle>{title || 'My Product'}</DrawerTitle>
         <ActionDrawerGeneral drawer>
-          <CustomSearch placeholder="Search Product" />
-          <CustomSearch placeholder="Search Fund" />
+          <CustomSearch placeholder="Add Product" onSelect={this.onSelectProduct} />
+          <CustomSearch placeholder="Search Fund" type="fund" />
         </ActionDrawerGeneral>
 
         <FundTable columns={this.columns} data={data} />
