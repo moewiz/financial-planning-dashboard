65e2ec861e06224bd1a12f846d25cae390e7b22f
Khoa Nguyen <meo.wizard@gmail.com>
2019-10-08 10:03:32 +0700

    Implement functionalites for Step 1
    - Textarea reflects to the data in formik
    - Handle onChange value should update the value in formik
    - Style textarea

--- src/components/DocumentsPage/DocumentsStep1/DocumentsStep1.tsx
+++ src/components/DocumentsPage/DocumentsStep1/DocumentsStep1.tsx
@@ -1,37 +1,50 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { connect } from 'formik';
+import { debounce } from 'lodash';
 
-import {
-  TitleStep,
-  TitleStepSmall,
-  StepWrapper,
-} from '../styled';
-import {
-  DocumentsStep1Form,
-} from './styled';
+import { StepWrapper, TitleStep, TitleStepSmall } from '../styled';
+import { DocumentsStep1Form } from './styled';
 import { DocumentData, FormikPartProps } from '../DocumentsPage';
+import EditCell, { EditCellType } from '../../StrategyPage/Drawer/EditCell';
 
-class DocumentsStep1 extends React.PureComponent<FormikPartProps> {
-  public render(): JSX.Element {
-    const { formik } = this.props;
-    return (
-      <StepWrapper>
-        <TitleStep>Why did Ian & Deborah seek advice?</TitleStep>
-        <TitleStepSmall>Your purpose for seeking advice</TitleStepSmall>
-        <DocumentsStep1Form>
-          <textarea
-            placeholder="What are your reasons for seeking financial advice? For example,
-            are you going through a life event,
-            such as starting a family or retrenchment, or are you planning for a future event such  as retirement?
-            You may wish to include any personal goals
-            you have. For example, do you want to spend
-            less time worrying about money and more time
-            with your family, or would you like to be in
-            a financial position to  reduce your working hours?" />
-        </DocumentsStep1Form>
+const DocumentsStep1 = (props: FormikPartProps) => {
+  const { formik } = props;
+  const [value, setValue] = useState(formik.values.step1);
+  useEffect(() => {
+    setValue(formik.values.step1);
+  }, [formik.values.step1]);
+  const placeholder =
+    'What are your reasons for seeking financial advice? ' +
+    'For example, are you going through a life event, such as starting a family or retrenchment, or ' +
+    'are you planning for a future event such  as retirement? You may wish to include any personal goals you have. ' +
+    'For example, do you want to spend less time worrying about money and more time with your family, or would you ' +
+    'like to be in a financial position to  reduce your working hours?';
+  const debounceEdit = useCallback(
+    debounce((val, name) => {
+      formik.setFieldValue(name, val);
+    }, 500),
+    [],
+  );
+  const onChange = (val: any, name: string) => {
+    setValue(val);
+    debounceEdit(val, name);
+  };
+
+  return (
+    <StepWrapper>
+      <TitleStep>Why did Ian & Deborah seek advice?</TitleStep>
+      <TitleStepSmall>Your purpose for seeking advice</TitleStepSmall>
+      <DocumentsStep1Form>
+        <EditCell
+          name={'step1'}
+          value={value}
+          onChange={onChange}
+          type={EditCellType.textarea}
+          placeholder={placeholder}
+        />
+      </DocumentsStep1Form>
     </StepWrapper>
-    );
-  }
-}
+  );
+};
 
 export default connect<{}, DocumentData>(DocumentsStep1);
