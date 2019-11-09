79e01965d9c8fa4a3955981645116f39b4268dcc
Khoa Nguyen <meo.wizard@gmail.com>
2019-11-09 08:47:24 +0700

    Fix override not showing strikethrough

--- src/components/DocumentsPage/styled.ts
+++ src/components/DocumentsPage/styled.ts
@@ -174,7 +174,7 @@ export const StepWrapper = styled.section`
   }
   .strikethrough {
     .strategy-item {
-      .ant-input.edit-cell {
+      .ant-input {
         text-decoration: line-through;
       }
     }
