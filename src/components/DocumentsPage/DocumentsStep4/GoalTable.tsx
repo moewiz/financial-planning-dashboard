c244c9d5ec39fb2c08ce6b43f499370af1df09cf
Khoa Nguyen <meo.wizard@gmail.com>
2019-11-12 16:48:42 +0700

    Revert "Fix change word "replace" to "rebalance" (#127)"
    This reverts commit 833ac75c69605b91ae5638202020fcfcb6f9efc2.

--- src/components/DocumentsPage/DocumentsStep4/GoalTable.tsx
+++ src/components/DocumentsPage/DocumentsStep4/GoalTable.tsx
@@ -5,6 +5,7 @@ import { Table } from 'antd';
 import uuid from 'uuid';
 
 import { Record, Row, StepProps } from '../DocumentsPage';
+import { TitleStep } from '../styled';
 import { EditCellType } from '../../StrategyPage/Drawer/EditCell';
 import GoalEdit from './GoalEdit';
 import { priorityOptions } from '../../../enums/options';
